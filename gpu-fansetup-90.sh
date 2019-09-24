3321f2e4fa3a4c4db4d23f40aeb0e7cffde24863
IraqNoPhobia <21281010+IraqNoPhobia@users.noreply.github.com>
2019-09-24 15:53:09 -0500

    Add files via upload

---
+++ gpu-fansetup-90.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+sudo nvidia-smi -pm 1
+sudo nvidia-smi -acp 0
+sudo /usr/bin/nvidia-settings -a GPUPowerMizerMode=1 -a GPUFanControlState=1 -a GPUTargetFanSpeed=90 -a GPUGraphicsClockOffset[3]=100
